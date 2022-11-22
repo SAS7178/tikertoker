adfaed42c48ff6598332331c4557af2562d52303
Stephen Smith <StephenASmith7178@gmail.com>
2022-11-22 16:33:20 -0500

    sanity backend implimented

--- tiktoker-backend/schemas/schema.js
+++ tiktoker-backend/schemas/schema.js
@@ -6,7 +6,6 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import post from './post';
 import comment from './comment';
 import user from './user';
-import comment from './comment';
 import postedBy from './postedBy';
 
 // Then we give our schema to the builder and provide the result to Sanity
